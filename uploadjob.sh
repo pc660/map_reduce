6dcccafd31f2206929d469071fb28f42844a178c
pc660 <pc660@hotmail.com>
2014-07-20 17:57:28 -0400

    1

--- uploadjob.sh
+++ uploadjob.sh
@@ -1,6 +1,6 @@
-java -cp bin: main example.wordcount 0
-java -cp bin: main example.wordcount 1
-java -cp bin: main example.wordcount 2
-java -cp bin: main example.wordcount 3
-java -cp bin: main example.wordcount 4
-java -cp bin: main example.wordcount 5
+java -cp bin: main example.wordcount 0 127.0.0.1 10001
+java -cp bin: main example.wordcount 1 127.0.0.1 10001
+java -cp bin: main example.wordcount 2 127.0.0.1 10001
+java -cp bin: main example.wordcount 3 127.0.0.1 10001
+java -cp bin: main example.wordcount 4 127.0.0.1 10001
+java -cp bin: main example.wordcount 5 127.0.0.1 10001
